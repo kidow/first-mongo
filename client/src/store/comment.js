14396b6cee3d527355c6868dbe550cd33dfdee31
kidow <wcgo2ling@hanmail.net>
2018-11-04 22:22:20 +0900

    redux store set

--- client/src/store/comment.js
+++ client/src/store/comment.js
@@ -2,10 +2,37 @@ import { Map } from 'immutable'
 import { createAction, handleActions } from 'redux-actions'
 import { pender } from 'redux-pender'
 
-const initialState = {
+const CHANGE_INPUT = 'comment/CHANGE_INPUT'
+const GET_COMMENT = 'comment/GET_COMMENT'
+const POST_COMMENT = 'comment/POST_COMMENT'
 
-}
+export const changeInput = createAction(CHANGE_INPUT)
+export const getComment = createAction(GET_COMMENT)
+export const postComment = createAction(POST_COMMENT)
+
+const initialState = Map({
+  commenter: '',
+  comment: ''
+})
 
 export default handleActions({
-
+  [CHANGE_INPUT]: (state, action) => {
+    const { name, value } = action.payload
+    return state.set(name, value)
+  },
+  ...pender({
+    type: GET_COMMENT,
+    onSuccess: (state, action) => {
+      const { comment, commenter } = action.payload.data
+      return state.set('commenter', commenter)
+                  .set('comment', comment)
+    }
+  }),
+  ...pender({
+    type: POST_COMMENT,
+    onSuccess: (state, action) => {
+      const { _id } = action.payload.data
+      return state.set('commentId', _id)
+    }
+  })
 }, initialState)
--- client/src/store/user.js
+++ client/src/store/user.js
@@ -2,10 +2,40 @@ import { Map } from 'immutable'
 import { createAction, handleActions } from 'redux-actions'
 import { pender } from 'redux-pender'
 
-const initialState = {
+const CHANGE_INPUT = 'user/CHANGE_INPUT'
+const GET_USER = 'user/GET_USER'
+const POST_USER = 'user/POST_USER'
 
-}
+export const changeInput = createAction(CHANGE_INPUT)
+export const getUser = createAction(GET_USER)
+export const postUser = createAction(POST_USER)
+
+const initialState = Map({
+  name: '',
+  age: '',
+  error: '',
+  married: ''
+})
 
 export default handleActions({
-
+  [CHANGE_INPUT]: (state, action) => {
+    const { name, value } = action.payload
+    return state.set(name, value) 
+  },
+  ...pender({
+    type: GET_USER,
+    onSuccess: (state, action) => {
+      const { name, age, married } = action.payload.data
+      return state.set('name', name)
+                  .set('age', age)
+                  .set('married', married)
+    }
+  }),
+  ...pender({
+    type: POST_USER,
+    onSuccess: (state, action) => {
+      const { _id } = action.payload.data
+      return state.set('userId', _id)
+    }
+  })
 }, initialState)
